746d31c93e74a787e8e4a4bb15a91f6d001609ec
kjh311 <kjh311@gmail.com>
2025-04-07 10:50:42 -0600

    first

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
