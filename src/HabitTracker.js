5a0d84b1edd23b5f350ed2fe43790cb6a645e2ba
kjh311 <kjh311@gmail.com>
2025-04-08 12:11:03 -0600

    add habit

--- src/HabitTracker.js
+++ src/HabitTracker.js
@@ -4,15 +4,26 @@ import ReactCalendarHeatmap from "react-calendar-heatmap";
 
 export default function HabitTracker() {
   const [habits, setHabits] = useState([]);
+  const [habitInput, setHabitInput] = useState("");
+
   const [name, setName] = useState("");
   const [nameInput, setNameInput] = useState("");
-  const [newHabit, setNewHabit] = useState("");
 
   useEffect(() => {
     const storedUserName = localStorage.getItem("habitTrackerUserName");
     if (storedUserName) {
       setName(storedUserName);
     }
+
+    const storedHabits = localStorage.getItem("habitTrackerHabits");
+    if (storedHabits) {
+      setHabits(JSON.parse(storedHabits));
+    }
+
+    // localStorage.removeItem("habitTrackerHabits");
+    // localStorage.removeItem("habitTrackerUserName");
+
+    console.log("Habits", habits);
   }, []);
 
   //ADD USER NAME
@@ -26,19 +37,41 @@ export default function HabitTracker() {
     setNameInput("");
   };
 
+  //ADD HABIT
+  const handleAddHabit = (e) => {
+    e.preventDefault();
+
+    if (!habitInput.trim()) return;
+
+    const newHabit = {
+      id: Date.now(),
+      name: habitInput.trim(),
+    };
+
+    localStorage.setItem(
+      "habitTrackerHabits",
+      JSON.stringify([...habits, newHabit])
+    );
+    setHabits((prev) => [...prev, newHabit]);
+
+    setHabitInput("");
+  };
+
   return (
     <div>
-      <h2>Habit Tracker</h2>
-
       <div>
         {name ? (
-          `Welcome,   ${name}!`
+          `Hello,   ${name}!`
         ) : (
-          <div>
+          <div className="card">
             <p>
-              `Welcome to Habit Tracker. Please enter your name to get started:`
+              Welcome to Habit Tracker. <br />
+              Please enter your name to get started:
             </p>
-            <form className="border rounded p-2 m-2" onSubmit={handleAddName}>
+            <form
+              className="border border-black rounded-lg p-2 m-2 "
+              onSubmit={handleAddName}
+            >
               <input
                 className="border rounded p-2 m-2"
                 type="text"
@@ -47,12 +80,42 @@ export default function HabitTracker() {
                 onChange={(e) => setNameInput(e.target.value)}
               />
               <br />
-              <button className="p-2 m-2 bg-blue-400 hover:bg-blue-600">
+              <button className="p-2 m-2 bg-blue-400 hover:bg-blue-600 border border-black rounded-lg">
                 Submit Name
               </button>
             </form>
           </div>
         )}
+      </div>
+
+      {/* add habit */}
+
+      <form className="card" onSubmit={handleAddHabit}>
+        <input
+          type="text"
+          className="border rounded-lg p-2 m-2"
+          value={habitInput}
+          placeholder="Enter a new habit:"
+          onChange={(e) => setHabitInput(e.target.value)}
+        />
+        <br />
+        <button
+          className="p-2 m-2 border rounded-lg bg-blue-400 hover:bg-blue-700 hover:text-white"
+          type="submit"
+        >
+          Add Habit
+        </button>
+      </form>
+
+      {/* show habits */}
+      <div className="card">
+        {habits.length > 0
+          ? habits.map((habit, id) => (
+              <div key={id}>
+                {habit.name}, ID: {habit.id}
+              </div>
+            ))
+          : "No Habits"}
       </div>
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export default function Navbar() {
   return (
-    <div className="border p-2 bg-blue-400">Welcome to Habit Tracker!</div>
+    <div className="border p-2 bg-blue-400">
+      <h1 className="text-lg text-bold">Welcome to Habit Tracker!</h1>
+    </div>
   );
 }
